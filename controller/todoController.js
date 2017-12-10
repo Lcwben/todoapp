381eb79a4527062a075dc84e4cc857a034fc6563
Lcwben <sizailcw@qq.com>
2017-12-10 22:52:21 +0800

    1.Modify todo.ejs 2.Adding code to get method and post method in Controller.

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,12 +1,20 @@
+var bodyParser = require('body-parser');
+
+var dataArr = [{title:'测试数据1。'}];
+
 module.exports = (app) => {
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+
     //获取数据
     app.get('/todo', (req, res) => {
-       res.render('todo');
+       res.render('todo',{list:dataArr});
     });
 
     //传递数据
     app.post('/todo', (req, res) => {
-
+        console.log(req.body);
+        dataArr.push(req.body);
     });
 
     //删除数据
